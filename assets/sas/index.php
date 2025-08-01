bf13a08c7e6ba0caad9caa21e3da66ea6ea37dc4
alyssa <alyssa.companioni@gmail.com>
2025-08-01 11:42:07 -0400

    adds quality statements to add to the description statements for each featured work. Final commit before CTS-115

--- assets/sas/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Main Application Entry Point
- * 
- * Redirects visitors to the public directory
- */
-
-// CHANGE: Added proper spacing around parentheses and single quotes for consistency
-header('Location: public/');
